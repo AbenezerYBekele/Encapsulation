fb2fd5cf1381a0a440ab3e1f6aa47f07f99d6e81
Abenezer Y Bekele <113382918+AbenezerYBekele@users.noreply.github.com>
2025-01-31 23:25:23 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        BackAccount backAccount= new BackAccount("Wonder Woman",2500);
-        backAccount.withdraw(300);
-        backAccount.deposit(500);
-        backAccount.withdraw(1100);
+        BankAccount bankAccount= new BankAccount("Wonder Woman",2500);
+        bankAccount.withdraw(300);
+        bankAccount.deposit(500);
+        bankAccount.withdraw(1100);
 
-        System.out.println(backAccount.getOwner());
-        System.out.println(backAccount.getBalance());
-
-
+        System.out.println("Owner: "+ bankAccount.getOwner());
+        System.out.println("Balance: "+ bankAccount.getBalance());
     }
 }
